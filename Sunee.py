758e232efd7013e11c2e55279ffe7298b8b1da04
Sunny Sheikh <87214270+Sunny-Sheikh@users.noreply.github.com>
2021-07-12 11:24:44 +0600

    Add files via upload

---
+++ Sunee.py
@@ -0,0 +1,5 @@
+#!C:\Users\Sunee\AppData\Local\Programs\Python\Python37-32\python.exe
+print("Content-Type: text/html\n")
+print ("Hello Python Web Browser!! This is cool!!")
+print ("This Work\'s")
+
